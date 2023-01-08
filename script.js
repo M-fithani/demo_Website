bd8e8b638b5703cf63d26249f72c5e28a122cb6f
M-fithani <115891316+M-fithani@users.noreply.github.com>
2023-01-08 11:13:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// $(document).ready(function() {
+//     $('.banner_class').slick({
+//         dots: true,
+//         arrows:false,      
+//         autoPlay: true,
+//         autoPlaySpeed : 1000,
+        
+  
+//     });
+//   });
+//   $('.banner_class').slick({
+//     slidesToShow: 2,
+//     responsive: [{
+//       breakpoint: 500,
+//       settings: {
+//         slidesToShow: 1,
+//       }
+//     }]
+//   });
+
+ 
